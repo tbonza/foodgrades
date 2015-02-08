74e0b5267671fd8cd0622bb46ed14720fdf7dcf0
Dylan <dylanjportelance@gmail.com>
2015-02-08 12:44:55 -0500

    update api with resource renames and url restructure

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask.ext.restful import reqparse, abort, Api, Resource
+from flask.ext.restful import abort, Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
@@ -36,30 +36,30 @@ with open('violations.txt') as file:
         violation_id += 1 
 file.close()
 
-class Restaurants(Resource):
+class AllRestaurantsAPI(Resource):
 
     def get(self):
         if not restaurants:
             abort(404, message="No Restaurants")
         return restaurants
 
-class Restaurant(Resource):
+class RestaurantsAPI(Resource):
 
     def get(self, restaurant_id):
         if restaurant_id not in restaurants:
             abort(404, message="Restaurant {}, doesn't exist".format(restaurant_id))
         return restaurants[restaurant_id]
 
-class Violation(Resource):
+class ViolationsAPI(Resource):
 
     def get(self, restaurant_id, violation_id):
         if violation_id not in restaurants[restaurant_id]["violations"]:
             abort(404, message="Resturant {}, Violation {}, doesn't exist".format(restaurant_id, violation_id))    
         return restaurants[restaurant_id]["violations"][violation_id]
 
-api.add_resource(Restaurants, '/restaurants')
-api.add_resource(Restaurant, '/restaurants/<restaurant_id>')
-api.add_resource(Violation, '/restaurants/<restaurant_id>/violations/<violation_id>')
+api.add_resource(AllRestaurantsAPI, 'api/v1.0/restaurants')
+api.add_resource(RestaurantsAPI, 'api/v1.0/restaurants/<restaurant_id>')
+api.add_resource(ViolationsAPI, 'api/v1.0/restaurants/<restaurant_id>/violations/<violation_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
