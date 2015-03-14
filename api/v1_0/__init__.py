02db9cb9c325afecc9968d256d4a9345375f38d8
tbonza <bonza.email@gmail.com>
2015-03-14 14:36:57 -0400

    revisions continue

--- api/v1_0/__init__.py
+++ api/v1_0/__init__.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, g
+from ..errors import not_found
 
 api = Blueprint('api', __name__)
 
+@api.errorhandler(400)
+def bad_request_error(e):
+    return bad_request('invalid request')
+
+
+@api.errorhandler(404)
+def not_found_error(e):
+    return not_found('item not found')
 
 # do this last to avoid circular dependencies
-from . import names
+from . import restaurants, violations
--- api/v1_0/restaurants.py
+++ api/v1_0/restaurants.py
@@ -1,8 +1,42 @@
 from ..decorators import json
 from ..models import model_v1
 from . import api
+from flask import abort, jsonify
+
+@api.route('/restaurants/', methods=['GET'])
+@json
+def get_all_restaurants():
+    restaurants = model_v1()
+    if not restaurants:
+        abort(404)
+    return restaurants
+
+@api.route('/restaurants/name/<restaurant_name>', methods=['GET'])
+@json
+def get_restaurant_name(restaurant_name):
+    restaurants = model_v1()
+    restaurantsWithName = []
+    for key in restaurants:
+        if restaurants[key]["BusinessName"] == restaurant_name:
+            restaurantsWithName.append(restaurants[key])
+    return restaurantsWithName
+
+@api.route('/hmm/<articleid>', methods=['GET'])
+#@json
+def get_hmm(articleid):
+    return 'you are reading ' + articleid
+    
+
+
+
 
 @api.route('/names/', methods=['GET'])
 @json
 def get_names():
     return {'names': 'hello world'}
+
+@api.route('/test/', methods=['GET'])
+@json
+def get_missing():
+    
+    return abort(404)
